4feb13309f1531e02ae978ae76136b7ecb3812b1
Krishna Sameer <krishnasameer51@gmail.com>
2023-02-12 23:21:53 +0530

    Some random chikubum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Hi welcome to the guessing game!!");
+    println!("Enter a number");
+
+    let mut number = String::new();
+
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read Number");
+
+    println!("You have guessed : {number}");
 }
